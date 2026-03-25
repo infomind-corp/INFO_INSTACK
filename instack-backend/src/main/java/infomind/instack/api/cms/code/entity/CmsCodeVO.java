929db842cf19d15ea8ee85605a0b33e074ffb478
김병주 <atker14@gmail.com>
2026-03-25 14:39:07 +0900

    feat: cms/code init

---
+++ instack-backend/src/main/java/infomind/instack/api/cms/code/entity/CmsCodeVO.java
@@ -0,0 +1,36 @@
+package infomind.instack.api.cms.code.entity;
+
+import infomind.instack.api.common.entity.BaseAuditVO;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * CMS 공통 코드 VO.
+ * <p>CMS_COM_CODE 테이블 매핑. 코드 계층 구조를 self-referencing FK로 관리한다.</p>
+ * <p>최상위 코드는 UP_CD = '0' 으로 처리한다.</p>
+ */
+@Getter
+@Setter
+public class CmsCodeVO extends BaseAuditVO {
+    private String cd;            // 코드 (PK)
+    private String upCd;          // 상위 코드 (PK, self-referencing, 최상위='0')
+    private String cdNm;          // 코드 명
+    private Integer cdLvl;        // 코드 레벨 (1=최상위, 2=하위)
+    private String useYn;         // 사용 여부
+    private Integer cdOrd;        // 코드 순서
+    private String refCd1;        // 참조 코드1
+    private String refCd2;        // 참조 코드2
+    private String refCd3;        // 참조 코드3
+    private String refCd4;        // 참조 코드4
+    private String refCd5;        // 참조 코드5
+    private Double refNum1;       // 참조 숫자1
+    private Double refNum2;       // 참조 숫자2
+    private Double refNum3;       // 참조 숫자3
+    private Double refNum4;       // 참조 숫자4
+    private Double refNum5;       // 참조 숫자5
+    private String cdRmk;         // 코드 비고
+
+    public CmsCodeVO() {
+        super();
+    }
+}
---
+++ instack-backend/src/main/java/infomind/instack/api/cms/code/service/impl/CmsCodeServiceImpl.java
@@ -0,0 +1,93 @@
+package infomind.instack.api.cms.code.service.impl;
+
+import org.egovframe.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import infomind.instack.api.cms.code.dao.CmsCodeDao;
+import infomind.instack.api.cms.code.entity.CmsCodeVO;
+import infomind.instack.api.cms.code.model.CodeRequest;
+import infomind.instack.api.cms.code.model.CodeResponse;
+import infomind.instack.api.cms.code.service.CmsCodeService;
+import infomind.instack.api.common.exception.BizException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * CMS 공통 코드 서비스 구현.
+ */
+@Service
+@RequiredArgsConstructor
+public class CmsCodeServiceImpl extends EgovAbstractServiceImpl implements CmsCodeService {
+
+    private static final String TOP_CD = "0";
+
+    private final CmsCodeDao cmsCodeDao;
+
+    @Override
+    public List<CodeResponse> listTop() {
+        return cmsCodeDao.selectCodeListByUpCd(TOP_CD);
+    }
+
+    @Override
+    public List<CodeResponse> listByUpCd(String upCd) {
+        return cmsCodeDao.selectCodeListByUpCd(upCd);
+    }
+
+    @Override
+    public CodeResponse select(String cd, String upCd) {
+        return cmsCodeDao.selectCodeById(cd, upCd)
+                .orElseThrow(() -> new BizException("코드를 찾을 수 없습니다", HttpStatus.NOT_FOUND));
+    }
+
+    @Override
+    @Transactional
+    public void insertTop(CodeRequest request) {
+        CmsCodeVO vo = new CmsCodeVO();
+        BeanUtils.copyProperties(request, vo);
+        vo.setCdLvl(1);
+        vo.setUpCd(TOP_CD);
+        cmsCodeDao.insertCode(vo);
+    }
+
+    @Override
+    @Transactional
+    public void insertSub(String upCd, CodeRequest request) {
+        // 상위 코드 존재 여부 확인
+        cmsCodeDao.selectCodeById(upCd, TOP_CD)
+                .orElseThrow(() -> new BizException("상위 코드를 찾을 수 없습니다", HttpStatus.NOT_FOUND));
+
+        CmsCodeVO vo = new CmsCodeVO();
+        BeanUtils.copyProperties(request, vo);
+        vo.setCdLvl(2);
+        vo.setUpCd(upCd);
+        cmsCodeDao.insertCode(vo);
+    }
+
+    @Override
+    @Transactional
+    public void update(String cd, String upCd, CodeRequest request) {
+        // 코드 존재 여부 확인
+        cmsCodeDao.selectCodeById(cd, upCd)
+                .orElseThrow(() -> new BizException("코드를 찾을 수 없습니다", HttpStatus.NOT_FOUND));
+
+        CmsCodeVO vo = new CmsCodeVO();
+        BeanUtils.copyProperties(request, vo);
+        cmsCodeDao.updateCode(cd, upCd, vo);
+    }
+
+    @Override
+    @Transactional
+    public void delete(String cd, String upCd) {
+        // 코드 존재 여부 확인
+        cmsCodeDao.selectCodeById(cd, upCd)
+                .orElseThrow(() -> new BizException("코드를 찾을 수 없습니다", HttpStatus.NOT_FOUND));
+
+        // 하위 코드 삭제
+        cmsCodeDao.deleteCodeByUpCd(cd);
+        // 코드 삭제
+        cmsCodeDao.deleteCode(cd, upCd);
+    }
+}
