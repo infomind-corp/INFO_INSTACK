0d187d7235003ceeff6d037964357015b37ded06
김병주 <atker14@gmail.com>
2026-03-26 10:51:40 +0900

    수정

--- instack-backend/src/main/java/infomind/instack/api/config/RestConfig.java
+++ instack-backend/src/main/java/infomind/instack/api/config/RestConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.client.HttpComponentsClientHttpRequestFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
@@ -18,12 +19,9 @@ public class RestConfig {
      */
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-        return restTemplateBuilder
-                .setClientRequestFactoryBuilder(
-                    new HttpComponentsClientHttpRequestFactoryBuilder()
-                        .withConnectTimeout(Duration.ofMillis(3000))
-                        .withReadTimeout(Duration.ofMillis(3000))
-                )
-                .build();
+            SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+            factory.setConnectTimeout(3000);
+            factory.setReadTimeout(3000);
+            return new RestTemplate(factory);
     }
 }
