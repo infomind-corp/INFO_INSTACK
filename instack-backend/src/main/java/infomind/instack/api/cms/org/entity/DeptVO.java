77cf5df74187ed0b7805ca98eab68246fb7fd029
김병주 <atker14@gmail.com>
2026-03-26 14:40:19 +0900

    수정

--- instack-backend/src/main/java/infomind/instack/api/cms/org/entity/DeptVO.java
+++ instack-backend/src/main/java/infomind/instack/api/cms/org/entity/DeptVO.java
@@ -16,6 +16,7 @@ public class DeptVO extends BaseAuditVO {
     private String deptNm;        // 부서 명
     private Integer ord;          // 순서
     private String useYn;         // 사용 여부
+    private String searchKeyword; // 검색 키워드 (DB 컬럼 아님)
 
     public DeptVO() {
         super();
